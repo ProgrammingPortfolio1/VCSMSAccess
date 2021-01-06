c839e0728e76819a91d3af572cf0d1cc814da307
ProgrammingPortfolio1 <70398262+ProgrammingPortfolio1@users.noreply.github.com>
2021-01-07 00:41:16 +0200

    Initial commit

---
+++ VCSMSAccess/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace VCSMSAccess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String query = textBox1.Text;
+            String connStr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=library.mdb;User Id=admin;Password=;";
+            OleDbConnection conn = new OleDbConnection(connStr);
+            OleDbCommand cmd = new OleDbCommand(query, conn);
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Command Executed");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            conn.Close();
+        }
+    }
+}
